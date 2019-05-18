7d810c6e17926f22f81624b9d68408fbea67cfc7
athira <athiravnair321@gmail.com>
2019-05-18 17:50:46 +0530

    updating

--- Abcd/src/com/app/Android.java
+++ Abcd/src/com/app/Android.java
@@ -4,7 +4,7 @@ public class Android extends Mobile{
 	public void android() {
 		System.out.println("from android..");
 		System.out.println("implification..");
-
+//github
 	}
 public static void main(String[] args) {
 	Android ad= new Android();
